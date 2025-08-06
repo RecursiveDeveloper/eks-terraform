d180386d64ff542bec182891e50bcc2bc34326ea
Jhoan Jesus Ortiz Sandoval <63681573+RecursiveDeveloper@users.noreply.github.com>
2025-08-06 16:34:57 -0500

    Delete modules/eks/outputs.tf

--- modules/eks/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "cluster_oidc_issuer_url" {
-  description = "value of the OIDC issuer URL for the EKS cluster"
-  value = module.eks.cluster_oidc_issuer_url
-}
-
-output "cluster_oidc_provider_arn" {
-  description = "value of the OIDC provider ARN for the EKS cluster"
-  value = module.eks.oidc_provider_arn
-}
