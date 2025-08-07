005707548f447974c2d725027962749bd9f33c64
RecursiveDeveloper <johanjesus133@hotmail.com>
2025-08-07 18:10:32 -0500

    ebs-csi-driver added to eks - 2

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -10,7 +10,7 @@ module "eks" {
     eks-pod-identity-agent = {}
     kube-proxy             = {}
     vpc-cni                = {}
-    ebs-csi-driver         = {}
+    aws-ebs-csi-driver     = {}
   }
 
   enable_cluster_creator_admin_permissions = true
